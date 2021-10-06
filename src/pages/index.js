1cdf6c4783780228cd28d24b28ff72076f369284
Cody Barr <cody.barr@gmail.com>
2021-10-06 08:43:02 -0400

    feat: add upcoming elections modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SEO from 'components/seo'
 import InviteForm from 'components/inviteform'
+import Modal from 'components/modal'
 
 const IndexPage = () => {
   return (
     <main className="h-full flex flex-col items-center justify-center p-8">
       <SEO title="Home" isFront={true} />
       <InviteForm />
+      <Modal />
     </main>
   )
 }
