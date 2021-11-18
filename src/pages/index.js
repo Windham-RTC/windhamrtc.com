df2a0fd7f08acee7af9be0e672e2bc808ce3c0cb
Cody Barr <cody.barr@gmail.com>
2021-11-18 14:03:29 -0500

    feat: remove election notification modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import SEO from 'components/seo'
 import InviteForm from 'components/inviteform'
-import Modal from 'components/modal'
+// import Modal from 'components/modal'
 
 const IndexPage = () => {
   return (
-    <main className="h-full flex flex-col items-center justify-center p-8">
+    <main className="flex flex-col items-center justify-center h-full p-8">
       <SEO title="Home" isFront={true} />
       <InviteForm />
-      <Modal />
+      {/* <Modal /> */}
     </main>
   )
 }
