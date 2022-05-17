622ea3361415b6313cc90bc02bbd4500d8fa2028
Cody Barr <cody.barr@gmail.com>
2022-05-17 14:01:51 -0400

    feat: add meetings page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,6 +12,7 @@ function classNames(...classes) {
 
 const NAV_LINKS = [
   ['Home', '/'],
+  ['Meetings', '/meetings'],
   ['Contact', '/contact'],
 ]
 
@@ -37,7 +38,7 @@ const Navbar = ({ location }) => {
       <Disclosure as="nav" className="bg-white shadow">
         {({ open }) => (
           <>
-            <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="w-full px-4 mx-auto sm:px-6 lg:px-8">
               <div className="flex justify-between h-16">
                 <div className="flex">
                   <motion.div
@@ -47,7 +48,7 @@ const Navbar = ({ location }) => {
                   >
                     <Link
                       to="/"
-                      className="flex-shrink-0 flex items-center space-x-5"
+                      className="flex items-center flex-shrink-0 space-x-5"
                     >
                       <StaticImage
                         width={50}
@@ -55,7 +56,7 @@ const Navbar = ({ location }) => {
                         src="../images/elephant.png"
                         alt="Windham Connecticut Republican Town Committee"
                       />
-                      <h2 className="font-bold text-rtc-red text-lg">
+                      <h2 className="text-lg font-bold text-rtc-red">
                         Windham RTC
                       </h2>
                     </Link>
@@ -69,14 +70,14 @@ const Navbar = ({ location }) => {
                     ))}
                   </div>
                 </div>
-                <div className="-mr-2 flex items-center sm:hidden">
+                <div className="flex items-center -mr-2 sm:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      <XIcon className="block w-6 h-6" aria-hidden="true" />
                     ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                      <MenuIcon className="block w-6 h-6" aria-hidden="true" />
                     )}
                   </Disclosure.Button>
                 </div>
